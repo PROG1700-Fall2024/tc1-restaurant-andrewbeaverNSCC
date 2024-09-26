f8b414d7d4f631cbafc752c1593ee2971e519f53
Your Name <w0402993@nscc.ca>
2024-09-26 11:18:31 -0300

    Finished tech check 1. All calcualtions are completed and correct

--- TechCheck1-RestaurantBill.py
+++ TechCheck1-RestaurantBill.py
@@ -1,6 +1,10 @@
 #PROG 1700 – Tech Check #1
 #Variables, Operators, Input/Output & Casting
 
+#Student Name:    Andrew Beaver
+#Program Title:  Restaurant Bill
+#Description: Tech Check 1   
+
 # Restaurant Bill 
 # You will create a console-based Python program that will calculate the amount of the tip, the tax, and the 
 # total amount of a restaurant bill. The program will prompt the user to input the original amount of the bill. 
@@ -10,11 +14,24 @@
 
 def main():
     # YOUR CODE STARTS HERE, each line must be indented (one tab)
+    #Welcome User to the program
+    print("Hello, welcome to the Bill Total calculator. We will calculate the total price with the tax and tip.")
+    #Prompt user to enter th eprice
+    billPrice = float(input("\nPlease enter the price of the original bill: "))
 
+    #Do math to calculate the tax and tip
+    billTax = billPrice * 0.15
+    billTip = billPrice * 0.20
+    totalPrice = billPrice + billTax + billTip
 
+    #display tax
+    print("Your tax is: ${0:,.2f}".format(billTax))
+    
+    #display tip
+    print("Your tip is: ${0:,.2f}".format(billTip))
 
-
-
+    #display total
+    print("Your total is: ${0:,.2f}".format(totalPrice))
 
 
 
